33a7e2f52b0d2b37dc0ba0a4842e59b892321a09
Fabian Schwarzkopf <fabian.schwarzkopf@yworks.com>
2024-03-06 18:01:37 +0100

    Make astro doc configuration component specific

---
+++ docs/config.ts
@@ -0,0 +1,18 @@
+export const SITE = {
+  title: 'yFiles React Organization Chart Component',
+  description: '',
+  defaultLanguage: 'en_US',
+  twitter: '@yworks',
+  github: 'yWorks/react-yfiles-orgchart',
+  linkedin: 'yworks-gmbh'
+}
+
+export const OPEN_GRAPH = {
+  image: {
+    src: '',
+    alt: ''
+  },
+  twitter: ''
+}
+
+// TODO twitter card
