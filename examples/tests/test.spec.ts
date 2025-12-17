ed63378302e70957686b655fe79e267a81d8a54d
Michael Frischeisen-Köhler <michael.frischeisen-koehler@yworks.com>
2025-12-17 14:54:09 +0100

    Improve tests

--- examples/tests/test.spec.ts
+++ examples/tests/test.spec.ts
@@ -5,7 +5,22 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/yFiles Orgchart Component Examples/)
 })
 
+const ignoredConsoleLogs = [
+  '[vite]',
+  'React DevTools',
+  'running in development mode',
+  // favicon.ico - we should also check failing network requests
+  '404'
+]
+
 test('examples', async ({ page }) => {
+  page.on('console', msg => {
+    const text = msg.text()
+    if (!ignoredConsoleLogs.some(ignored => text.includes(ignored))) {
+      expect.soft(text, `Unexpected console output`).toBeUndefined()
+    }
+  })
+
   await page.goto('.')
 
   const gcLocator = page.locator('css=.yfiles-canvascomponent:not(.graph-overview-component)')
