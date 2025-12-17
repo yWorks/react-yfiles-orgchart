f3af17b1f1ae34b47e8c41881ada39c166ce42ff
Michael Frischeisen-Köhler <michael.frischeisen-koehler@yworks.com>
2025-12-17 11:07:04 +0100

    Make tests more consistent

--- examples/tests/test.spec.ts
+++ examples/tests/test.spec.ts
@@ -1,32 +1,32 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test'
 
 test('has title', async ({ page }) => {
-  await page.goto('.');
-  await expect(page).toHaveTitle(/yFiles Orgchart Component Examples/);
-});
+  await page.goto('.')
+  await expect(page).toHaveTitle(/yFiles Orgchart Component Examples/)
+})
 
 test('examples', async ({ page }) => {
-  await page.goto('.');
-  
+  await page.goto('.')
+
   const gcLocator = page.locator('css=.yfiles-canvascomponent:not(.graph-overview-component)')
   await expect(gcLocator).toHaveCount(1)
-  
-  const comboLocator = page.getByRole('combobox', {name: "examples"})
+
+  const comboLocator = page.getByRole('combobox', { name: 'examples' })
   await expect(comboLocator).toHaveCount(1)
-  
-  const options = comboLocator.getByRole("option")
+
+  const options = comboLocator.getByRole('option')
   const optionCount = await options.count()
   for (let i = 0; i < optionCount; i++) {
     const option = options.nth(i)
     const exampleName = await option.textContent()
-    console.log("Testing example", exampleName)
+    console.log('Testing example', exampleName)
     await comboLocator.selectOption(exampleName)
-    
+
+    // wait for the old gc to be gone
+    await page.waitForTimeout(100)
     const gcCount = await gcLocator.count()
     for (let i = 0; i < gcCount; i++) {
       await gcLocator.nth(i).click()
     }
   }
-
-});
-
+})
